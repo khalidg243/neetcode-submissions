d8eb1face4b661188d642a2eb27a6aa9b5ba9dd7
Khalid Hamid <132845349+khalidg243@users.noreply.github.com>
2026-09-16 00:01:57 +0300

    Add: set-zeroes-in-matrix - submission-0

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        rows = []
+        cols = []
+
+        for row in range(0,len(matrix)):
+            for col in range(0,len(matrix[0])):
+                if matrix[row][col] == 0:
+                    if row not in rows:
+                        rows.append(row)
+                    if col not in cols:
+                        cols.append(col)
+        
+        for row in rows:
+            matrix[row] = [0] * len(matrix[0])
+        
+        for col in cols:
+            for i in range(len(matrix)):
+                matrix[i][col] = 0
+
+        
